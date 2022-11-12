638ca52f138629b2a293bc859cf587d4bd1e71cc
jonathan962 <vchavzpena@gmail.com>
2022-11-12 13:42:17 -0600

    se probo el repositorio remoto modificando el el homecontroller

--- E-Commerce/Controllers/HomeController.cs
+++ E-Commerce/Controllers/HomeController.cs
@@ -11,27 +11,6 @@ namespace E_Commerce.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        // modificamos el homecontroller para hacer la primera prueba 
     }
 }
