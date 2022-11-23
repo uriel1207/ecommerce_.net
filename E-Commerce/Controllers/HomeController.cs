54299a89f5daf375ed5f5603fca44a763173350d
JuanMa1220 <93569705+JuanMa1220@users.noreply.github.com>
2022-11-23 00:15:25 -0600

    cambios en  index.cshtml se agrego categorias en la parte del header

--- E-Commerce/Controllers/HomeController.cs
+++ E-Commerce/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace E_Commerce.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
